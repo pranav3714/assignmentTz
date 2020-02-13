fdac43878ebf9bab40cd0b92b3dfbb36498763b2
pranav3714 <51287393+pranav3714@users.noreply.github.com>
2020-02-13 18:54:38 +0530

    Modified to run locally

--- src/components/SearchHolder.js
+++ src/components/SearchHolder.js
@@ -14,7 +14,7 @@ let SearchHolder = (props) => {
         if (filter === "Name") {
             try {
                 setLoader(true)
-                let vals = await axios.get("https://votecommision.herokuapp.com/voter/search/name/" + val, { headers })
+                let vals = await axios.get("http://localhost:4000/voter/search/name/" + val, { headers })
                 setLoader(false)
                 setVals(vals.data.data)
             } catch (err) {
@@ -25,7 +25,7 @@ let SearchHolder = (props) => {
         else if (filter === "District") {
             try {
                 setLoader(true)
-                let vals = await axios.get("https://votecommision.herokuapp.com/voter/search/district/" + val, { headers })
+                let vals = await axios.get("http://localhost:4000/voter/search/district/" + val, { headers })
                 setLoader(false)
                 setVals(vals.data.data)
             } catch (err) {
@@ -36,7 +36,7 @@ let SearchHolder = (props) => {
         else if (filter === "EID") {
             try {
                 setLoader(true)
-                let vals = await axios.get("https://votecommision.herokuapp.com/voter/search/eid/" + val, { headers })
+                let vals = await axios.get("http://localhost:4000/voter/search/eid/" + val, { headers })
                 setLoader(false)
                 //console.log(vals.data.data)
                 setVals([vals.data.data])
